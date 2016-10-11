e9600facf11469956a267188e1bba1a64f4b2c96
Bernardo <bvieira@gmail.com>
2016-10-10 22:31:48 -0300

    reiniciando curso

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,10 @@
+// public/js/controllers/fotos-controller.js
+
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+    $scope.foto = {
+        titulo : 'Leão',
+        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    };
+
+});
